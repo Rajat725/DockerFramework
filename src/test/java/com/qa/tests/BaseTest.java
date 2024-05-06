2d320ba763396d499a6eadddb97538cd0ca129f0
Rajat725 <imrajat108@gmail.com>
2024-05-06 07:57:39 +0530

    Parameterize Browser

--- src/test/java/com/qa/tests/BaseTest.java
+++ src/test/java/com/qa/tests/BaseTest.java
@@ -4,12 +4,15 @@ import com.qa.browser.LocalBrowser;
 import com.qa.driver.DriverManager;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 public class BaseTest {
 
+    @Parameters({"browser"})
     @BeforeMethod(alwaysRun = true)
-    public void setUpBrowser() {
-        LocalBrowser.setUpBrowser("Chrome");
+    public void setUpBrowser(@Optional("Chrome") String browser) {
+        LocalBrowser.setUpBrowser(browser);
         LocalBrowser.launchWebsite("https://opensource-demo.orangehrmlive.com/");
     }
 
