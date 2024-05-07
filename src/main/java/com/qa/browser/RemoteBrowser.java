4fa7a520e5c5c83fa6a533d4be88d44a4e3fa5bf
Rajat725 <imrajat108@gmail.com>
2024-05-07 09:32:27 +0530

    Added Remote Browser Class and Change Pom .xml to run tests on selenium grid

--- src/main/java/com/qa/browser/RemoteBrowser.java
+++ src/main/java/com/qa/browser/RemoteBrowser.java
@@ -1,4 +1,36 @@
 package com.qa.browser;
 
+import com.qa.driver.DriverManager;
+import lombok.SneakyThrows;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+
 public class RemoteBrowser {
+
+    public static void launchWebsite(String url)
+    {
+        DriverManager.getDriver().get(url);
+    }
+
+    @SneakyThrows
+    public static void setupBroswer() {
+
+        Capabilities capabilities = null;
+
+        if (System.getProperty("browser").equalsIgnoreCase("chrome")) {
+            capabilities = new ChromeOptions();
+        }
+        if (System.getProperty("browser").equalsIgnoreCase("edge")) {
+            capabilities = new EdgeOptions();
+        }
+
+
+        URL url = new URL("http://localhost:4444");
+        DriverManager.setDriver(new RemoteWebDriver(url, capabilities));
+
+    }
 }
--- src/test/java/com/qa/tests/BaseTest.java
+++ src/test/java/com/qa/tests/BaseTest.java
@@ -1,6 +1,7 @@
 package com.qa.tests;
 
 import com.qa.browser.LocalBrowser;
+import com.qa.browser.RemoteBrowser;
 import com.qa.driver.DriverManager;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -12,8 +13,16 @@ public class BaseTest {
     @Parameters({"browser"})
     @BeforeMethod(alwaysRun = true)
     public void setUpBrowser(@Optional("Chrome") String browser) {
-        LocalBrowser.setUpBrowser(browser);
-        LocalBrowser.launchWebsite("https://opensource-demo.orangehrmlive.com/");
+
+        if (System.getProperty("selenium.grid.enabled").equalsIgnoreCase("false")) {
+            LocalBrowser.setUpBrowser(browser);
+            LocalBrowser.launchWebsite("https://opensource-demo.orangehrmlive.com/");
+        } else {
+            RemoteBrowser.setupBroswer();
+            RemoteBrowser.launchWebsite("https://opensource-demo.orangehrmlive.com/");
+
+        }
+
     }
 
     @AfterMethod(alwaysRun = true)
