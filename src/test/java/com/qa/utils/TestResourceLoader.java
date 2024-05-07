d81f88364d6b994be00595db96ef6871e2738e16
Rajat725 <imrajat108@gmail.com>
2024-05-07 07:57:36 +0530

    Amend Resource Loader Code

---
+++ src/test/java/com/qa/utils/TestResourceLoader.java
@@ -0,0 +1,29 @@
+package com.qa.utils;
+
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+
+public class TestResourceLoader {
+
+    private static final Logger log = LoggerFactory.getLogger(TestResourceLoader.class);
+
+    public static InputStream getResource(String path) throws Exception {
+        log.info("reading resource from location: {}", path);
+        //InputStream stream =  ResourceLoader.class.getClassLoader().getResourceAsStream(path);
+        final InputStream stream = TestResourceLoader.class.getClassLoader().getResourceAsStream(path);
+        if (Objects.nonNull(stream)) {
+            log.info("Class Path");
+            return stream;
+        }
+        log.info("Project Path");
+        return Files.newInputStream(Path.of(path));
+    }
+
+}
