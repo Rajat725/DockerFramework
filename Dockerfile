7856f96557f3eef1850c8fbd85e65b4fc9b8dd5b
Rajat725 <imrajat108@gmail.com>
2024-05-11 09:24:46 +0530

    Added Runner.sh and Amend Docker File

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,13 @@
 FROM bellsoft/liberica-openjdk-alpine:17
+RUN apk add curl jq
 WORKDIR /home/DockerFramework
 ADD target/docker-framework-package ./
-ENTRYPOINT java -Dseleniumgridenabled=${GRID} \
-           -Dhuburl=${HUBURL} \
-           -cp 'libs/*' \
-           org.testng.TestNG \
-           -threadcount ${THREAD_COUNT} \
-           suites-xml/${XMLFILE}
+ADD runner.sh runner.sh
+ENTRYPOINT sh runner.sh
+#ENTRYPOINT java -Dseleniumgridenabled=${GRID} \
+#           -Dhuburl=${HUBURL} \
+#           -cp 'libs/*' \
+#           org.testng.TestNG \
+#           -threadcount ${THREAD_COUNT} \
+#           suites-xml/${XMLFILE}
 
---
+++ runner.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#-------------------------------------------------------------------
+#  This script expects the following environment variables
+#     HUB_HOST
+#     BROWSER
+#     THREAD_COUNT
+#     TEST_SUITE
+#-------------------------------------------------------------------
+
+# Let's print what we have received
+echo "-------------------------------------------"
+echo "HUBURL        : ${HUBURL:-http://192.168.1.7:4444}"
+echo "GRID          : ${GRID:-true}"
+echo "THREAD_COUNT  : ${THREAD_COUNT:-2}"
+echo "TEST_SUITE    : ${XMLFILE:-testng.xml}"
+echo "-------------------------------------------"
+
+# Do not start the tests immediately. Hub has to be ready with browser nodes
+echo "Checking if hub is ready..!"
+count=0
+while [ "$( curl -s ${HUBURL:-http://192.168.1.7:4444}/status | jq -r .value.ready )" != "true" ]
+do
+  count=$((count+1))
+  echo "Attempt: ${count}"
+  if [ "$count" -ge 30 ]
+  then
+      echo "**** HUB IS NOT READY WITHIN 30 SECONDS ****"
+      exit 1
+  fi
+  sleep 1
+done
+
+# At this point, selenium grid should be up!
+echo "Selenium Grid is up and running. Running the test...."
+
+# Start the java command
+java -Dseleniumgridenabled=${GRID} \
+           -Dhuburl=${HUBURL} \
+           -cp 'libs/*' \
+           org.testng.TestNG \
+           -threadcount ${THREAD_COUNT} \
+           suites-xml/${XMLFILE}
